612472b8e71ed5261c3355da33fcb8e57c393940
avantenaidoo <60886136+avantenaidoo@users.noreply.github.com>
2025-07-03 13:51:40 +0200

    update: favicon + feat: add apple-mobile-web-app-title meta tag to layout for iOS support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="apple-mobile-web-app-title" content="MC Fencing" />
+      </head>
       <body
         className="antialiased"
       >
