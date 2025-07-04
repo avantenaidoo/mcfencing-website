39141f05633eb711914f439037bdd6f63e3a5944
avantenaidoo <60886136+avantenaidoo@users.noreply.github.com>
2025-07-04 09:20:16 +0200

    Update Navbar links to use Next.js Link with route and hash navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,56 +1,49 @@
 import Image from "next/image";
-import type { FC } from "react";
+import Link from "next/link";
 
-const Navbar: FC = () => {
+export default function Navbar() {
   return (
-    <nav className="sticky top-0 z-50 w-full bg-black text-white px-4 py-4 shadow-md">
-      <div className="max-w-5xl mx-auto flex justify-between items-center">
+    <nav className="fixed top-0 left-0 w-full z-50 bg-black text-white shadow-md">
+      <div className="max-w-5xl mx-auto px-4 py-4 flex justify-between items-center">
         <div className="flex items-center space-x-4">
-          <Image
-            src="/images/MCFencingLogo.png"
-            alt="MC Fencing Logo"
-            width={48}
-            height={48}
-          />
+          <Image src="/images/MCFencingLogo.png" alt="MC Fencing Logo" width={48} height={48} />
           <span className="font-bold text-xl">MC Fencing</span>
         </div>
         <ul className="flex space-x-6 text-lg">
           <li>
-            <a
-              href="#home"
+            <Link
+              href="/"
               className="hover:text-blue-400 transition-all duration-300 ease-in-out transform hover:scale-110"
             >
               Home
-            </a>
+            </Link>
           </li>
           <li>
-            <a
-              href="#services"
+            <Link
+              href="/#services"
               className="hover:text-blue-400 transition-all duration-300 ease-in-out transform hover:scale-110"
             >
               Services
-            </a>
+            </Link>
           </li>
           <li>
-            <a
-              href="#about"
+            <Link
+              href="/about"
               className="hover:text-blue-400 transition-all duration-300 ease-in-out transform hover:scale-110"
             >
               About
-            </a>
+            </Link>
           </li>
           <li>
-            <a
-              href="#contact"
+            <Link
+              href="/#contact"
               className="hover:text-blue-400 transition-all duration-300 ease-in-out transform hover:scale-110"
             >
               Contact
-            </a>
+            </Link>
           </li>
         </ul>
       </div>
     </nav>
   );
-};
-
-export default Navbar;
+}
