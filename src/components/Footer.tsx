e971a2eb97d913e39efe15e520ffafcd5d705f05
avantenaidoo <60886136+avantenaidoo@users.noreply.github.com>
2025-07-09 13:08:26 +0200

    Updated version control v1.0.1

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,7 +14,7 @@ export default function Footer() {
           Barry-Dean IT Solutions
         </a>
       </div>
-      <div className="italic">V1.0.0</div>
+      <div className="italic">V1.0.1</div>
     </footer>
   );
 }
