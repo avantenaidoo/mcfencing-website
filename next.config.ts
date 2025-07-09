e00a3662402c448ace5823c530f186d86a8158be
avantenaidoo <60886136+avantenaidoo@users.noreply.github.com>
2025-07-09 11:41:50 +0200

    Deployment comments for IIS export +

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Uncomment output and images for static export deployment
+  // output: "export", // Only use for deploy for IIS - Enables static export (creates /out folder)
+  // images: {
+  //   unoptimized: true, // Required for next/image to work with static export
+  // },
+  // trailingSlash: true, // Optional: ensures URLs map cleanly to folders (important on IIS)
 };
 
 export default nextConfig;
